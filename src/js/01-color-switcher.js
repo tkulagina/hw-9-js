5b886c00e34547304b2b241513975b8b41087863
tkulagina <tkulagina1974@gmail.com>
2023-04-30 16:26:25 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,41 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
 
+//отримуємо доступ до кнопок
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+//отримуємо id таймеру
+let timerId = null;
+
+//додаємо слухача на кнопку Старт
+startBtn.addEventListener('click', onStartChangeColor);
+
+//функція змін коліру
+function onStartChangeColor() {
+    onBtnDisabled ()
+    timerId = setInterval(() => {
+    const color = getRandomHexColor();
+    document.body.style.backgroundColor =`${color}`;
+    }, 1000)
+};
+
+//додаемо слухача на кнопку Стоп
+stopBtn.addEventListener ('click', onStopChangeColor);
+
+//функція зупинки
+function onStopChangeColor () {
+    clearInterval(idInterval);
+    onBtnDisabled ()
+};
+//функція перемикання активної кнопки
+function onBtnDisabled () {
+    if (!startBtn.disabled) {
+      startBtn.disabled = true;
+      stopBtn.disabled = false
+    } else {
+      startBtn.disabled = false;
+      stopBtn.disabled = true
+    }
+  };
