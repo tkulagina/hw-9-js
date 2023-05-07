7b9ebee718a7971a67a4629a700019a99e0a79b0
tkulagina <tkulagina1974@gmail.com>
2023-05-07 14:44:55 +0300

    update task 02

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -51,12 +51,12 @@ function startTimer() {
 
 //функція обчіслювання
 function getDeltaTime() {
-    const timerId = setInterval(() => {
+    const timerId = setTimeout(() => {
         delta = SELECTED_DATE - Date.now();
         const dateOffset = convertMs(delta);
         
         if (delta <= 0) {
-             clearInterval(timerId);
+             clearTimeout(timerId);
              btnStart.disabled = false;
              dateTimeInput.disabled = false;                      
         } else {
